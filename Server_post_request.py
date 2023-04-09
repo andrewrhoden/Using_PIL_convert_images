cf5c8110ae01aae72183f7fac9af8066154c53bd
Andrew Rhoden <andrewrhoden22@gmail.com>
2023-04-08 23:46:22 -0400

    made changes to resolve 500 error

--- Server_post_request.py
+++ Server_post_request.py
@@ -1,29 +1,29 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
 import os
 import requests
 
- 
+# Set directory path and URL
+dir_path = "/data/feedback/"
+url = "http://104.154.51.98/feedback/"
 
-# Set the directory containing the feedback files
-feed_path = "/data/feedback"
+# Loop through each file in directory
+for file_name in os.listdir(dir_path):
+    # Define keys and dictionary to store feedback data
+    keys = ["title", "name", "date", "feedback"]
+    feedback_dict = {}
+    
+    # Open file and read each line to store in dictionary
+    with open(dir_path + file_name, "r") as file:
+        lines = file.readlines()
+        for i in range(len(keys)):
+            feedback_dict[keys[i]] = lines[i].strip()
 
-# Get a list of all .txt files in the feedback directory
-feedback_files = [file for file in os.listdir(feed_path) if file.endswith(".txt")]
+    # Send feedback dictionary as JSON in POST request to URL
+    response = requests.post(url, json=feedback_dict)
 
-# Iterate over each file and extract the information
-for file in feedback_files:
-    with open(os.path.join(feed_path, file)) as f:
-        lines = f.readlines()
-        feedback = {
-            "title": lines[0].strip(),
-            "name": lines[1].strip(),
-            "date": lines[2].strip(),
-            "feedback": lines[3].strip(),
-        }
-        response = requests.post("http://<url>/feedback", json=feedback)
-        if response.status_code != 201:
-            print(f"Error posting feedback for {file}: {response.status_code} {response.text}")
-            
-            
-             
+    # Check if request was successful and print error message if not
+    if response.status_code == 201:
+        print(f"Feedback {file_name} posted successfully!")
+    else:
+        print(f"Error posting feedback {file_name}: Status code {response.status_code}")
